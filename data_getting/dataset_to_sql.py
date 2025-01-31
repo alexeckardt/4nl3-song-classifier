ac14dcfcf4492d2da96a49e18762ee444c6a04b9
Alex Eckardt <realalexeck@gmail.com>
2025-01-31 17:57:31 -0500

    Function to Func

--- data_getting/dataset_to_sql.py
+++ data_getting/dataset_to_sql.py
@@ -10,23 +10,32 @@ def main():
     files = [f'{path}/dataset_{x+1}.json' for x in fs]
     for fileName in files:
 
-        dbObj = {}
+        dbObj = []
         with open(fileName, 'r') as f:
             dbObj = json.load(f)
 
         # LOad
         df = pd.DataFrame(dbObj)
 
-        print(df.head())
+        # Add columns required by api
+        df['recognized'] = -1 #Add a column
+        df['name'] = df['title']
+        df['topic1'] = ''
+        df['topic2'] = ''
+        df['decade'] = -1
+        df['id'] = list(range(len(dbObj)))
+
+        # Drop the Name
+        df.drop_columns(['title']);
 
         conn = sqlite3.connect(fileName.replace('.json', '.db'))  # This will create songs.db if it doesn't exist
 
         # Save DataFrame to SQLite
-        df.to_sql("songs", conn, if_exists="replace", index=False)  # 'songs' is the table name
+        df.to_sql("Song", conn, if_exists="replace", index=False)  # 'songs' is the table name
 
         # Commit and close the connection
         conn.commit()
         conn.close()
 
-
-main()
+if __name__ == '__main__':
+    main()
