d0496d2f151317f5cb6defa62000a517c59c9a82
Alex Eckardt <realalexeck@gmail.com>
2025-03-13 19:27:45 -0400

    new data

--- datasets/completed/combine.py
+++ datasets/completed/combine.py
@@ -13,17 +13,40 @@ def create_song_hash(df):
 for path in paths:
     if path.endswith('.db'):
 
+        # Load the dataset as dataframe
+        name = path.replace('dataset-', '').split('.db')[0]
+        print(name)
+
         # 
         conn = sqlite3.connect(f'./datasets/completed/{path}')
         dbDf = pd.read_sql_query("SELECT * FROM Song", conn);
         print(df.head())
         conn.close()
 
+        dbDf['annotator'] = name
+
         # Combine
         df = pd.concat([df, dbDf], ignore_index=True)
 
 df = create_song_hash(df)
 
+unique = set()
+rows = 0
+for _, row in df.iterrows():
+    song_hash = row['song_hash']
+    unique.add(song_hash)
+    rows += 1
+
+print(f'Unique Songs: {len(unique)}')
+print(f'Total Rows: {rows}')
+
+d = rows - len(unique)
+print(f'Duplicated Songs Found:{d}')
+print(f'Total Proportion: {d/rows}')
+
+
 # Write output
 df.to_csv('./datasets/combined.csv', index=False)
+df.drop(columns=['lyrics'], inplace=True)
+df.to_csv('./datasets/combined_no_lyrics.csv', index=False)
 print(df.head())
