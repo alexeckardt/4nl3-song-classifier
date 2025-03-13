2a856a566e5b4c52cd2472f66287540b5650306f
Alex Eckardt <realalexeck@gmail.com>
2025-03-13 19:28:53 -0400

    Renamed file

--- model_training/README.md
+++ model_training/README.md
@@ -7,3 +7,10 @@ To run, ensure you are in the home directory. Then run the following command:
 ```bash
 py model_training/kappa_analysis.py
 ```
+
+Do to an error in our dataset splitting, only datasets_1 (`dataset-christian.db`) and dataset_2 (`dataset-stanley.db`)
+contained duplicated entires. Every requirement layed out in the project description was met, (15% of the total instances are duplicated, just not split up correctly).
+The error was not caught until after the data was split and recombined.
+
+Thus, we decided to use Cohens' Kappa (Pairwise Agreement between two Annotators). This is a measure of how much two annotators agree on a set of items -- which in this case is 
+between the duplicated 15% of the two datasets.
--- model_training/kappa_analysis.py
+++ model_training/kappa_analysis.py
@@ -1,8 +1,83 @@
 #Run the analasys
+import pandas as pd
 from dataset_loading import load_dataset
+
+annotatori = 'christian'
+annotatorj = 'stanley'
+
+
+def construct_kappa_df(df):
+
+    # Get all that overlap
+    df = df[df.duplicated('song_hash', keep=False)].copy()
+
+    # Remove Un-needed Columns
+    df.drop(columns=['artist','year','lyrics','name','id',], inplace=True)
+
+    # Passback
+    return df
+
+# Constuct O(1) lookup for the annotator based on the song hash.
+def construct_agreement_dict(unique_df, annotator, column):
+
+    #Resplit out the data per annotator
+    df = unique_df[unique_df['annotator'] == annotator] #only get this person's stuff
+
+    # Construct the Dictionary
+    out = {}
+
+    #Loop Over the df
+    for i in range(0, len(df)):
+        
+        # Get the hask
+        song_hash = df.iloc[i]['song_hash']
+        result = df.iloc[i][column]
+
+        # Store the Result
+        out[song_hash] = result
+
+    #Constructs O(1) lookup
+    return out
+
+
+
+def cohen_kappa(unique_pairwise_df, annotatori, annotatorj, column='topic1'):
+
+    # Get
+    iResults = construct_agreement_dict(unique_pairwise_df, annotatori, column)
+    jResults = construct_agreement_dict(unique_pairwise_df, annotatorj, column)
+
+    # Print
+    for key in iResults:
+        iAnswer = iResults[key]
+        jAnswer = jResults[key]
+
+        print(f'{key} {iAnswer} {jAnswer}')
 
 
 if __name__ == '__main__':
 
+    # Load dataset
     df = load_dataset();
-    print(df.head())
+    # annotators = df['annotator'].unique()
+
+    # Get the Annotators with Duplicated Data (SEE ./model_training/README.md)
+    dfI = df[df['annotator'] == annotatori]
+    dfJ = df[df['annotator'] == annotatorj]
+
+    # Repeat Expreiment per Columns
+    columns_interested = ['topic1'] # ['topic1', 'topic2', 'decade', 'decade']
+    for column in columns_interested:
+
+        # Combine
+        print(f'Comparing Agreement between {annotatori} and {annotatorj} on {column}')
+
+        # Construct Pairwise Dataframes
+        combinedDf = pd.concat([dfI, dfJ], ignore_index=True)
+        uniqueDf = construct_kappa_df(combinedDf)
+
+        print(uniqueDf.head())
+        
+        # Compute the Kappa
+        cohen_kappa(uniqueDf, annotatori, annotatorj, column)
+            
