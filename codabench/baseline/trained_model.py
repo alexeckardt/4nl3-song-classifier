e4ba31a82e75fc6a245d840f7480d676841c6c08
NeelDidACartwheel <neeljos24@gmail.com>
2025-03-17 03:58:48 -0400

    Models

--- codabench/baseline/trained_model.py
+++ codabench/baseline/trained_model.py
@@ -1,24 +1,46 @@
-import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 class Model:
     def __init__(self):
-        self.classifier = LogisticRegression()
+        self.vectorizer = CountVectorizer()
+        # Use multinomial logistic regression with the lbfgs solver.
+        self.model = LogisticRegression(max_iter=1000, multi_class='multinomial', solver='lbfgs')
 
     def fit(self, X, y):
-        """ Train the model.
-
+        """Train the model.
+        
         Args:
-            X: Training data matrix of shape (num-samples, num-features), type np.ndarray.
-            y: Training label vector of shape (num-samples), type np.ndarray.
+            X: Training data list of strings.
+            y: Training label matrix of shape (num_samples, 10), one-hot encoded.
         """
-        self.classifier.fit(X, y)
+        X = X.to_numpy().ravel()
+        X_transformed = self.vectorizer.fit_transform(X)
+
+        # Convert one-hot encoded labels to integer class indices.
+        y = y.to_numpy()
+        y_labels = np.argmax(y, axis=1)
+        
+        self.model.fit(X_transformed, y_labels)
 
     def predict(self, X):
-        """ Predict labels.
-
+        """Predict the labels for the given data.
+        
         Args:
-          X: Data matrix of shape (num-samples, num-features) to pass to the model for inference, type np.ndarray.
+            X: List of strings to predict.
+            
+        Returns:
+            Predicted class indices of shape (num_samples,).
         """
-        y = self.classifier.predict(X)
-        return y
+        X = X.to_numpy().ravel()
+        predicts = []
+        for x in X:
+            x_transformed = self.vectorizer.transform([x])
+            pred = self.model.predict_proba(x_transformed)
+            top_two_indices = np.argsort(pred[0])[-2:][::-1]
+            one_hot_pred = [0] * 11
+            one_hot_pred[top_two_indices[0]] = 1
+            one_hot_pred[top_two_indices[1]] = 1
+            predicts.append(one_hot_pred)
+        return predicts
