06987972c39251c64f236a13f1e2d00d802c00a5
ericzz12 <80360373+ericzz12@users.noreply.github.com>
2025-03-18 17:20:56 -0400

    data

--- codabench/feedback_phase/split_data.py
+++ codabench/feedback_phase/split_data.py
@@ -7,10 +7,8 @@ data = pd.read_csv('./codabench/feedback_phase/combined.csv')
 # train on lyrics
 train = data['lyrics']
 
-# one hot encoding for topics 1 and 2, then adding the encodings together to have 2 topics to be classified
-t1 = pd.get_dummies(data['topic1'])
-t2 = pd.get_dummies(data['topic2'])
-test = t1.add(t2, fill_value=0).astype(int)
+data['topic_codes'] = pd.Categorical(data['topic1']).codes
+test = data['topic_codes']
 
 # split data to train and test set
 train_data, test_data, train_label, test_label = train_test_split(train, test, test_size=0.2, random_state=42)
@@ -25,5 +23,5 @@ test_data.to_csv('./codabench/feedback_phase/input_data/testing_data.csv', heade
 
 # write labels
 train_label.to_csv('./codabench/feedback_phase/input_data/training_label.csv', header=False, index=False)
-val_label.to_csv('./codabench/feedback_phase/input_data/validataion_data.csv', header=False, index=False)
+val_label.to_csv('./codabench/feedback_phase/input_data/validation_label.csv', header=False, index=False)
 test_label.to_csv('./codabench/feedback_phase/reference_data/testing_label.csv', header=False, index=False)
